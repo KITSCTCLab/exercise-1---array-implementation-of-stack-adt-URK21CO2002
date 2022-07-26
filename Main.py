05706c91cbb389950063f2f02d1a7d0911d2a5ea
URK21CO2002 <110015064+URK21CO2002@users.noreply.github.com>
2022-07-26 12:46:42 +0530

    Create Main.py

--- Main.py
+++ Main.py
@@ -6,20 +6,36 @@ class Stack:
 
     def is_empty(self):
         # Write code here
+        return self.top<=-1
 
     def is_full(self):
         # Write code here
+        return (len(self.stack)-1)==self.top
 
     def push(self, data):
         if not self.is_full():
             # Write code here
+             print("stack is full...")
+        else:
+            self.top=self.top+1
+            self.stack[self.top]=int(input('enter the data:'))
+            print(self.stack)
 
     def pop(self):
         if not self.is_empty():
             # Write code here
+             print("stack is empty...")
+        else:
+            self.stack[self.top]=None
+            self.top=self.top-1
+            print(self.stack)
 
     def status(self):
         # Write code here
+        if(self.isempty()):
+            print("stack is empty...")
+        else:
+            print(self.stack[self.top])
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
